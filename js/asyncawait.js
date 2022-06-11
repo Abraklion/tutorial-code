bbba72fea46b8e57a4d8a72511260291eb1a15ce
Abraklion <abraklion@gmail.com>
2022-06-11 14:30:04 +0500

    5: запрос GET к серверу методом Async/awate и вывод пунктов меню

---
+++ js/asyncawait.js
@@ -0,0 +1,202 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  /* ================================================================= */
+  /* ============================= FETCH ============================= */
+  /* ================================================================= */
+
+  const forms = document.querySelectorAll('form');
+
+  // статусы выполнения операций
+  const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...'
+  };
+
+  forms.forEach(item => {
+    bindPostData(item);
+  });
+
+  // отправить данные POST запросом
+  const postData = async (url, data) => {
+    let res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.text(); // если принимаете json : json()
+  };
+
+  // получить данные GET запросом
+  const getData = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  // делает отправку формы на сервер мeтодом Async/await
+  function bindPostData(form) {
+    // form - тип: узел; формат: форма;
+
+    form.addEventListener('submit', (e) => {
+
+      e.preventDefault();
+
+      let statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      // обьект помогает работать с формами
+      const formData = new FormData(form);
+
+      // если надо конвертировать обьект formData в Json вариант 1
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      // если надо конвертировать обьект formData в Json вариант 2
+      // const object = {};
+      // formData.forEach(function(value, key){
+      //   object[key] = value;
+      // });
+
+      /* начало запроса */
+
+      // делаем запрос на сервер через Async/awate
+      postData('server.php', json)
+        .then(data => {
+
+        console.log(data);
+
+        showThanksModal(message.success);
+
+        statusMessage.remove();
+
+      }).catch((e) => {
+
+        console.log(e.message)
+
+        showThanksModal(message.failure);
+
+      }).finally(() => {
+
+        form.reset();
+
+      });
+
+    });
+
+  }
+
+  // оповещения в модальном окне после отправки формы
+  function showThanksModal(message) {
+    // message - тип: строка; формат: статус выполнения запроса;
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    showModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+    document.querySelector('.modal').append(thanksModal);
+
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      hideModal();
+    }, 4000);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* ================================================================= */
+  /* ================  для работы модального окна  =================== */
+  /* ================================================================= */
+
+  const modalTrigger = document.querySelectorAll('[data-modal-btn]'),
+    modal = document.querySelector('[data-modal]');
+
+  // вешаем на все кнопки события клик, по клику показывам модальное окно
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', (e) =>{
+
+      e.preventDefault()
+      showModal()
+
+    })
+  })
+
+  // вешаем на пространство вокруг модального окна события клик, по клику скрываем модальное окно
+  modal.addEventListener('click',(e) => {
+
+    if(e.target === modal || e.target.getAttribute('data-close') === "") {
+      hideModal();
+    }
+
+  })
+
+  // вешаем на документ события нажата клавиша
+  document.addEventListener('keydown', (e) => {
+
+    // если клавиша нажата и ее код Escape и у модального окна есть класс show, скрываем модальное окно
+    if(e.code === "Escape" && modal.classList.contains('show')){
+      hideModal();
+    }
+
+  })
+
+  // показывает модальное окно
+  function showModal(){
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+
+    document.body.style.overflow = 'hidden'
+
+  }
+
+  // скрывает модальное окно
+  function hideModal(){
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+
+    document.body.style.overflow = ''
+  }
+
+})
---
+++ js/asyncawateposts.js
@@ -0,0 +1,95 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  /* ================================================================= */
+  /* ========================== FETCH POST =========================== */
+  /* ================================================================= */
+
+  // класс который разпечатывает 1 элемент меню
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+  }
+
+  // делает асинхроный GET запрос и возвращает Json
+  async function getResource(url) {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  // ловим json и распечатываем меню в цикле
+  getResource('db.json')
+    .then(data => {
+      data['menu'].forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+      });
+    });
+
+
+  /* распечатать меню функциональным подходом
+
+  getResource('http://localhost:3000/menu')
+      .then(data => createCard(data));
+
+  function createCard(data) {
+      data.forEach(({img, altimg, title, descr, price}) => {
+          const element = document.createElement('div');
+
+          element.classList.add("menu__item");
+
+          element.innerHTML = `
+              <img src=${img} alt=${altimg}>
+              <h3 class="menu__item-subtitle">${title}</h3>
+              <div class="menu__item-descr">${descr}</div>
+              <div class="menu__item-divider"></div>
+              <div class="menu__item-price">
+                  <div class="menu__item-cost">Цена:</div>
+                  <div class="menu__item-total"><span>${price}</span> грн/день</div>
+              </div>
+          `;
+          document.querySelector(".menu .container").append(element);
+      });
+  }
+
+  */
+})
