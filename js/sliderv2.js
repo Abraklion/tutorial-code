d21ff9c5e6c9ea78a517e2692ad6c5f67128d87e
Abraklion <abraklion@gmail.com>
2022-06-17 17:15:58 +0500

    12: Слайдер вариант 2: модернизация: добавил функцию которая оставляет только числа в сторке

--- js/sliderv2.js
+++ js/sliderv2.js
@@ -92,11 +92,11 @@ window.addEventListener('DOMContentLoaded', function() {
 
   // вперед
   next.addEventListener('click', () => {
-    if (offset === (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+    if (offset === (deleteNotDigits(width) * (slides.length - 1))) {
       // скидываем смешения на первый слайдер
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -120,9 +120,9 @@ window.addEventListener('DOMContentLoaded', function() {
   prev.addEventListener('click', () => {
     if (offset === 0) {
       // скидываем смешения на последний слайдер
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -148,7 +148,7 @@ window.addEventListener('DOMContentLoaded', function() {
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -162,6 +162,11 @@ window.addEventListener('DOMContentLoaded', function() {
       dots[slideIndex-1].style.opacity = 1;
     });
   });
+
+  // удаляет из строки все кроме чисел
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
 
 })
 
