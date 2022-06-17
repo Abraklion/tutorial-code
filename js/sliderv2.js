a297a07bd9851ba286ba1ff9fc7ca46b5fb6486a
Abraklion <abraklion@gmail.com>
2022-06-17 13:56:01 +0500

    11: Слайдер вариант 2: модернизация: добавил точки (dots)

--- js/sliderv2.js
+++ js/sliderv2.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function() {
   let slideIndex = 1; // номер слайда
 
   const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),
@@ -41,7 +42,53 @@ window.addEventListener('DOMContentLoaded', function() {
   slides.forEach(slide => {
     slide.style.width = width;
   });
+
+  slider.style.position = 'relative';
   // стили конец
+
+  // Создания навигации по точкам - начало
+  const indicators = document.createElement('ol'),
+    dots = [];
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+    if (i === 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+  // Создания навигации по точкам - конец
 
   // вперед
   next.addEventListener('click', () => {
@@ -93,6 +140,27 @@ window.addEventListener('DOMContentLoaded', function() {
     } else {
       current.textContent =  slideIndex;
     }
+  });
+
+  // При клике на точку
+  dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slideIndex < 10) {
+        current.textContent =  `0${slideIndex}`;
+      } else {
+        current.textContent =  slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = ".5");
+      dots[slideIndex-1].style.opacity = 1;
+    });
   });
 
 })
